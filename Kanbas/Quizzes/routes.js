5bf457c61e2a3571347b33f17e44c39c51539a19
江梓铭 <jiangziming@jiangzimingdeMacBook-Air.local>
2024-07-30 00:55:14 -0400

    complete until quiz quetions editor

--- Kanbas/Quizzes/routes.js
+++ Kanbas/Quizzes/routes.js
@@ -2,6 +2,7 @@ import * as dao from "./dao.js";
 
 //TODO: add authorization for quizzes related operation
 export default function QuizRoutes(app) {
+  //Update quiz
   const updateQuiz = async (req, res) => {
     const { qid } = req.params;
     const quiz = req.body;
@@ -9,12 +10,14 @@ export default function QuizRoutes(app) {
     res.sendStatus(204);
   };
 
+  //Delete Quiz
   const deleteQuiz = async (req, res) => {
     const { qid } = req.params;
     const status = await dao.deleteQuiz(qid);
     res.sendStatus(200);
   };
 
+  //Create quiz
   const createQuiz = async (req, res) => {
     const { cid } = req.params;
     const courseID = cid;
@@ -25,6 +28,7 @@ export default function QuizRoutes(app) {
     res.send(createdQuiz);
   };
 
+  //Find all quizzes under one course
   const findQuizzesForCourse = async (req, res) => {
     const { cid } = req.params;
     const { user } = req; // TODO: need to include user in the request
@@ -44,23 +48,22 @@ export default function QuizRoutes(app) {
     }
   };
 
+  //Find quiz by id
   const findQuizById = async (req, res) => {
-    
     const {qid} = req.params;
     console.log("Getting quize for: ", qid);
     const quiz = await dao.findQuizById(qid);
     res.json(quiz);
   }
 
+  //Update the status of publish
   const updatePublishStatus = async (req, res) => {
     const { qid } = req.params;
     const { action } = req.query;
     if (!['publish', 'unPublish'].includes(action)) {
       return res.status(400).json({ message: 'Invalid action' });
     }
-  
     const publishStatus = action === 'publish';
-  
     try {
       const result = await dao.updateQuiz(qid, { published: publishStatus });
       if (result.nModified === 0) {
@@ -71,8 +74,86 @@ export default function QuizRoutes(app) {
       console.error(`Failed to ${action} quiz:`, error);
       res.status(500).json({ message: 'Internal server error' });
     }
-
   }
+
+  //Preview the quiz
+  const previewQuiz = async (req, res) => {
+    const {qid} = req.params;
+    try {
+      const quiz = await dao.findQuizById(qid);
+      if (!quiz) {
+        return res.status(404).json({ message: 'Quiz not found' });
+      }
+      res.json({ message: 'Quiz preview', quiz });
+    } catch (error) {
+      console.error('Error fetching quiz preview:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  //Edit the quiz
+  const editQuiz = async (req, res) => {
+    const {qid} = req.params;
+    try {
+      const result = await dao.updateQuiz(qid, req.body);
+      if (result.nModified === 0) {
+        return res.status(404).json({ message: 'Quiz not found' });
+      }
+      res.sendStatus(204);
+    } catch (error) {
+      console.error('Error editing quiz:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  //Add questions to quiz
+  const addQuestions = async (req, res) => {
+    try {
+      const { qid } = req.params;
+      const question = req.body;
+      const quiz = await dao.findQuizById(qid);
+      if (!quiz) {
+        return res.status(404).json({ message: 'Quiz not found' });
+      }
+      quiz.questions.push(question);
+      await quiz.save();
+      res.status(201).json(question);
+    } catch (error) {
+      console.error('Error adding question:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  //Update Questions in quiz
+  const updateQuestionsNoChoices = async (req, res) => {
+    const { qid, questionId } = req.params;
+    const updateData = req.body;
+    try {
+      const quiz = await dao.findQuizById(qid);
+      if (!quiz) {
+        return res.status(404).json({ message: 'Quiz not found' });
+      }
+      console.log("questionId: ", questionId)
+      const question = quiz.questions.id(questionId);
+      console.log("question: ", question);
+      if (!question) {
+        return res.status(404).json({ message: 'Question not found' });
+      }
+      question.set(updateData);
+      console.log("uodated question: ", question)
+      await quiz.save();
+      res.status(204).send();
+    } catch (error) {
+      console.error('Error updating question:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+/*TODO
+deleteQuestions: app.delete("api/quizzes/:qid/questions/:questionId)
+updateChoices: app.put("/api/quizzes/:qid/questions/:questionId/choices/choiceId")
+addChoices: app.post("/api/quizzes/:qid/questions/:questionId/choices")
+deleteChoices: app.delete("api/quizzes/:qid/questions/:questionId/choices/choiceId") */
 
   app.put("/api/quizzes/:qid", updateQuiz);
   app.delete("/api/quizzes/:qid", deleteQuiz);
@@ -80,4 +161,8 @@ export default function QuizRoutes(app) {
   app.get("/api/courses/:cid/quizzes", findQuizzesForCourse); //TODO: to test with user
   app.get("/api/quizzes/:qid", findQuizById);
   app.put("/api/quizzes/:qid/updatePublishStatus", updatePublishStatus);
+  app.get("/api/quizzes/:qid/preview", previewQuiz);
+  app.put("/api/quizzes/:qid/edit", editQuiz)
+  app.post("/api/quizzes/:qid/questions", addQuestions);
+  app.put("/api/quizzes/:qid/questions/:questionId", updateQuestionsNoChoices);
 }
--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,17 +1,99 @@
 import mongoose from "mongoose";
 
+const questionSchema = new mongoose.Schema({
+  questionTitle: {
+    type: String,
+    required: true
+  },
+  questionType: {
+    type: String,
+    enum: ['True/False', 'Multiple Choice', 'Fill in Multiple Blanks'],
+    default: 'Multiple Choice'
+  },
+  points: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 1
+  },
+  question: {
+    type: String,
+    required: true
+  },
+  choices: [{
+    optionText: String,
+    correct: { type: Boolean, default: false }
+  }]
+});
+
 const quizSchema = new mongoose.Schema({
   title: {
     type: String,
-    trim: true
+    trim: true,
+    default: 'New Quiz'
   },
   courseID:{
     type: String,
     required: true
   },
+  description:{
+    type: String
+  },
+  questions: [questionSchema],
   published:{
     type: Boolean,
-    required: true
+    default: false
+  },
+  quizType: {
+    type: String,
+    enum: ['Graded Quiz', 'Practice Quiz', 'Graded Survey', 'Ungraded Survey'],
+    default: 'Graded Quiz'
+  },
+  points: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  assignmentGroup: {
+    type: String,
+    enum: ['Quizzes', 'Exams', 'Assignments', 'Project'],
+    default: 'Quizzes'
+  },
+  shuffleAnswers: {
+    type: Boolean,
+    default: true
+  },
+  timeLimit: {
+    type: Number,
+    default: 20 // in minutes
+  },
+  multipleAttempts: {
+    type: Boolean,
+    default: false
+  },
+  howManyAttempts: {
+    type: Number,
+    default: 1
+  },
+  showCorrectAnswers: {
+    type: String,
+    default: ''
+  },
+  accessCode: {
+    type: String,
+    default: ''
+  },
+  oneQuestionAtATime: {
+    type: Boolean,
+    default: true
+  },
+  webcamRequired: {
+    type: Boolean,
+    default: false
+  },
+  lockQuestionsAfterAnswering: {
+    type: Boolean,
+    default: false
   },
   availableDate: {
     type: Date,
@@ -24,11 +106,7 @@ const quizSchema = new mongoose.Schema({
   dueDate: {
     type: Date
   },
-  points: {
-    type: Number,
-    min: 0
-  },
-  questions: {
+  totalPoints: {
     type: Number,
     min: 0
   },
@@ -37,7 +115,7 @@ const quizSchema = new mongoose.Schema({
     min: 0
   }
 }, 
-{ collection: "quizzes"}
+{ collection: "quizzes" }
 );
 
 export default quizSchema;
